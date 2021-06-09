a64c4020ff6f105b8835a52a0003790e051b2f64
saifur116 <67361625+saifur116@users.noreply.github.com>
2021-06-09 16:56:38 +0600

    d

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser=require('body-parser')
 require('dotenv').config()
 const ObjectI= require('mongodb').ObjectID
 const app = express()
-const port = process.env.PORT||5000
+const port =5000
 app.use(cors())
 app.use(bodyParser.json())
 
@@ -89,7 +89,8 @@ serviceCollection.find()
 
 
     
-app.listen(port=>port)
+app.listen(process.env.PORT|| port)
+
 
 
 
